c9e3d3acd14602cdfb4cb27aa8cccf455da425cd
Raphael <boo.rho@gmail.com>
2014-10-31 11:18:43 -0500

    where am I?

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@ class User < ActiveRecord::Base
 
 	def self.create_or_find_user_from_oauth(oauth_hash)
 	  user = self.find_by_email(oauth_hash.email)
+	  puts oauth_hash
 	  unless user
 	    user = self.create(name: oauth_hash.name,
 	                       gravatar: oauth_hash.gravatar,
-	                       email: oauth_hash.email)
+	                       email: oauth_hash.email,
+	                       cohort_id: oauth_hash.cohort_id)
 	  end
 	  user
 	end
