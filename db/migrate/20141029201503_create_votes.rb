d8451989b52fb63adb1a979f72a351d1247f830c
Shomari <Shomari.Ewing@gmail.com>
2014-10-31 16:39:30 -0500

    finshed controllers and added a few specs

--- db/migrate/20141029201503_create_votes.rb
+++ db/migrate/20141029201503_create_votes.rb
@@ -1,11 +1,13 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.integer :voteable_id
-      t.string  :voteable_type
+      t.integer :voteable_id #the id of the object that it is
+      t.string  :voteable_type #if its a review or place
       t.boolean :upvote?
 
       t.timestamps
     end
   end
+
+
 end
