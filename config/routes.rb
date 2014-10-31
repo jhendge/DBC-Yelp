095df6c2d3d8920e9853beecd7f4c947de56f5d6
Raphael <boo.rho@gmail.com>
2014-10-31 12:46:11 -0500

    adding pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
   get '/signin', :to => 'sessions#sign_in'
   get '/signout', :to => 'sessions#sign_out'
   get 'auth/:provider/callback', :to => 'sessions#auth'
 
 
-=======
-
-  
->>>>>>> master
   resources :categories, only: [:index] do
   end
 
 
-  root to: 'users#index'
+  root to: 'pages#index'
 
   resources :places do
     resources :photos
