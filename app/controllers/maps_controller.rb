ab9eb44d300e45385e539370e06b0ddbfcce098a
Josh Nixon <seriousfools@gmail.com>
2014-10-31 16:21:01 -0500

    map jquery v1 and map methods

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -12,20 +12,37 @@ class MapsController < ActionController::Base
 
 	def index
 
-		def searchify_string(input)
-			@map = input.gsub(/ /, '%20') 
-		end
+		# def searchify_string(input)
+		# 	@map = input.gsub(/ /, '%20') 
+		# end
 
-		search_input="chipotle"
+		search_input="307 lake street"
 
-		searchify_string(search_input)
+		@map = Map.searchify_string(search_input)
+
+		# @address = 
+
+
 
 
 		# @map="Chipotle%20Mexican%20Grill%2C%20West%20Lake%20Street"
 	end
 
 	def script
-		
+
+		# def distance_between_coordinates(first_coord, second_coord)
+		# 	#coord are an array of two numbers, [lat, long]
+		# 	lat_difference = first_coord[0] - second_coord[0]
+		# 	long_difference = first_coord[1] - second_coord[1] 
+			
+		# 	distance_squared = (lat_difference**2) + (long_difference**2)
+
+		# 	Math.sqrt(distance_squared)
+		# end
+
+
+
+		 @square = Map.directions_from_address_json("351 w hubbard st chicago, il","307 w lake street chicago, il")
 	end
 
 	def new
--- app/models/map.rb
+++ app/models/map.rb
@@ -2,8 +2,31 @@ class Map < ActiveRecord::Base
 	has_many :places
 
 
-	def searchify_string(input)
-		@map = input.gsub(/ /, '%20') + 
+	def self.searchify_string(input)
+		# URI::escape(input)
+		input.gsub(/ /, '%20')
+	end
+
+	def self.address_to_coordinates(address)
+		address_json = JSON.parse(Net::HTTP.get(URI("https://maps.googleapis.com/maps/api/geocode/json?address=#{URI::escape(address)}")))
+		address_json["results"][0]["geometry"]["location"]
+	end
+
+	def self.distance_json_coord(first_coord, second_coord)
+	  uri = URI("https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{URI::escape(first_coord)}&destinations=#{URI::escape(second_coord)}&key=AIzaSyBOE5A3JBjh3Ho8FJ4pcddwSgl3rHE-fQs&avoid=highways&mode=walking")
+	  JSON.parse(Net::HTTP.get(uri))
+	end
+
+	def self.distance_json_address(first_address, second_address)
+	  start_location = "#{first_address["lat"]},#{first_address["lng"]}"
+	  destinations_url = "#{self.address_to_coordinates(second_address)}"
+	  uri = URI("https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{URI::escape(start_location)}&destinations=#{URI::escape(destinations_url)}&key=AIzaSyBOE5A3JBjh3Ho8FJ4pcddwSgl3rHE-fQs&avoid=highways&mode=walking")
+	  JSON.parse(Net::HTTP.get(uri))
+	end
+
+	def self.directions_from_address_json(start_address, end_address, mode = "walking")
+	  uri = URI("https://maps.googleapis.com/maps/api/directions/json?origin=#{URI::escape(start_address)}&destination=#{URI::escape(end_address)}&key=AIzaSyBOE5A3JBjh3Ho8FJ4pcddwSgl3rHE-fQs&avoid=highways&mode=#{mode}")
+	  JSON.parse(Net::HTTP.get(uri))
 	end
 
 end
